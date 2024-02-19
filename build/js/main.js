5c2a3d14fd079c66a67d875ac02ec7226116615a
Luka Nozadze <nozadzeluka2002@gmail.com>
2024-02-19 13:36:45 +0400

    union types

--- build/js/main.js
+++ build/js/main.js
@@ -2,9 +2,14 @@
 let myName = 'Luka';
 let meaningOfLife;
 let isLoading;
+let album; //union type
 myName = 'Luka';
 meaningOfLife = 21;
 isLoading = true;
+album = 1941;
+album = 'Van Halen';
 const sum = (a, b) => {
     return a + b;
 };
+let postId; //union type
+let isActive; //union type
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 let myName:string = 'Luka';
 let meaningOfLife:number;
 let isLoading:boolean;
-
+let album:string|number; //union type
 
 myName = 'Luka';
 meaningOfLife = 21;
 isLoading = true;
+album = 1941;
+album = 'Van Halen';
 
 const sum = (a:number,b:number)=>{
   return a+b;
 }
+
+
+let postId:string|number; //union type
+let isActive:number|boolean //union type
