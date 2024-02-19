de69b84900b34bf2ef48bdd0262123bc19ac3308
Luka Nozadze <nozadzeluka2002@gmail.com>
2024-02-19 17:17:39 +0400

    difference between interface and type; literal types

--- src/main.ts
+++ src/main.ts
@@ -1,70 +1,17 @@
-let stringArr = ["one", "hey", "John"];
-
-let guitars = ["Strat", "Les Paul", 5150];
-
-let mixedData = ["EVH", 1984, true];
-
-stringArr[0] = "Luka";
-stringArr.push("bye");
-
-guitars[0] = 1042;
-
-guitars = stringArr;
-
-let test = [];
-let bands: string[] = [];
-bands.push("Arctic Monkeys");
-
-//tuple
-let myTuple: [string, number, boolean] = ["John", 42, true];
-
-//union typed array
-let mixed = ["John", 1, false];
-
-mixed = myTuple;
-
-///objects
-let myObj: object;
-myObj = [];
-console.log(typeof myObj); //still object
-
-const exampleObject = {
-  prop1: "Luka",
-  prop2: true,
-};
-
-exampleObject.prop2 = false;
-
+//Type Aliases
+type stringOrNumber = string | number;
+type stringOrNumberArray = stringOrNumber[];
 interface Guitarist{
   name?: string;
   active: boolean;
-  albums: (string | number)[];
+  albums: stringOrNumberArray;
 };
 
-let evh: Guitarist = {
-  name: "Eddie",
-  active: false,
-  albums: [1984, 5150, "OU812"],
-};
-let JP: Guitarist = {
-  name: "Jimmy",
-  active:true,
-  albums: [1442, "IV", "OU812"],
-};
+type userId = stringOrNumber;
 
-const greetGuitarist = (guitarist: Guitarist) => {
-  return `Hello ${guitarist.name?.toUpperCase()}`;
-};
-console.log(greetGuitarist(JP));
+// interface postId = stringOrNumber; //we cant do the same with interface 
 
-//enums
 
-enum Grade{
-  U=1,
-  D,
-  C,
-  B,
-  A
-}
-
-console.log(Grade.U)
+//Literal types
+let myName:'Dave' | 'John' | 'Amy';
+// myName = 'Rachel' //myName must be Dave or John or Amy
