a03b56a1482d0201b1e56f06bab92eca938ca0aa
Luka Nozadze <nozadzeluka2002@gmail.com>
2024-02-19 17:07:48 +0400

    object types

--- build/js/main.js
+++ build/js/main.js
@@ -3,14 +3,39 @@ let stringArr = ["one", "hey", "John"];
 let guitars = ["Strat", "Les Paul", 5150];
 let mixedData = ["EVH", 1984, true];
 stringArr[0] = "Luka";
-stringArr.push('bye');
+stringArr.push("bye");
 guitars[0] = 1042;
 guitars = stringArr;
 let test = [];
 let bands = [];
-bands.push('Arctic Monkeys');
+bands.push("Arctic Monkeys");
 //tuple
-let myTuple = ['John', 42, true];
+let myTuple = ["John", 42, true];
 //union typed array
-let mixed = ['John', 1, false];
+let mixed = ["John", 1, false];
 mixed = myTuple;
+///objects
+let myObj;
+myObj = [];
+console.log(typeof myObj); //still object
+const exampleObject = {
+    prop1: "Luka",
+    prop2: true,
+};
+exampleObject.prop2 = false;
+;
+let evh = {
+    name: "Eddie",
+    active: false,
+    albums: [1984, 5150, "OU812"],
+};
+let JP = {
+    name: "Jimmy",
+    active: true,
+    albums: [1442, "IV", "OU812"],
+};
+const greetGuitarist = (guitarist) => {
+    var _a;
+    return `Hello ${(_a = guitarist.name) === null || _a === void 0 ? void 0 : _a.toUpperCase()}`;
+};
+console.log(greetGuitarist(JP));
--- src/main.ts
+++ src/main.ts
@@ -5,20 +5,54 @@ let guitars = ["Strat", "Les Paul", 5150];
 let mixedData = ["EVH", 1984, true];
 
 stringArr[0] = "Luka";
-stringArr.push('bye');
+stringArr.push("bye");
 
 guitars[0] = 1042;
 
 guitars = stringArr;
 
 let test = [];
-let bands:string[] = [];
-bands.push('Arctic Monkeys');
+let bands: string[] = [];
+bands.push("Arctic Monkeys");
 
 //tuple
-let myTuple:[string,number,boolean] = ['John',42,true];
+let myTuple: [string, number, boolean] = ["John", 42, true];
 
 //union typed array
-let mixed = ['John',1,false];
+let mixed = ["John", 1, false];
 
 mixed = myTuple;
+
+///objects
+let myObj: object;
+myObj = [];
+console.log(typeof myObj); //still object
+
+const exampleObject = {
+  prop1: "Luka",
+  prop2: true,
+};
+
+exampleObject.prop2 = false;
+
+interface Guitarist{
+  name?: string;
+  active: boolean;
+  albums: (string | number)[];
+};
+
+let evh: Guitarist = {
+  name: "Eddie",
+  active: false,
+  albums: [1984, 5150, "OU812"],
+};
+let JP: Guitarist = {
+  name: "Jimmy",
+  active:true,
+  albums: [1442, "IV", "OU812"],
+};
+
+const greetGuitarist = (guitarist: Guitarist) => {
+  return `Hello ${guitarist.name?.toUpperCase()}`;
+};
+console.log(greetGuitarist(JP));
