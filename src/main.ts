59173e0b4b7e9f757b28f8c1b788e3ebdc33449d
Luka Nozadze <nozadzeluka2002@gmail.com>
2024-02-21 18:09:35 +0400

    use keyof keyword for declaring Student object keys

--- src/main.ts
+++ src/main.ts
@@ -16,3 +16,22 @@ console.log(todaysTransactions['Pizza']);
 let prop:string = 'Pizza';
 
 console.log(todaysTransactions[prop]);
+
+////////////////////
+
+interface Student {
+  // [key:string]:string | number | number[] | undefined
+   name:string,
+  GPA:number,
+  classes?:number[]
+}
+
+const student:Student = {
+name:'Doug',
+GPA:3.5,
+classes:[100,200]
+}
+
+for (const key in student) {
+  console.log(`${key}: ${student[key as keyof Student]}`)
+}
