80a3e9aa9e91aa45b5ed7bf00f5afee4b0a2d0ab
Luka Nozadze <nozadzeluka2002@gmail.com>
2024-02-21 17:57:12 +0400

    index signatures in typescript

--- build/js/main.js
+++ build/js/main.js
@@ -1,57 +1,11 @@
 "use strict";
-// class Coder{
-//     secondLang!:string
-//    constructor(public readonly name:string,public music:string,private age:number,protected lang:string = 'Typescript'){
-//     this.name = name;
-//     this.music = music;
-//     this.age = age;
-//     this.lang = lang;
-//    }
-class Guitarist {
-    constructor(name, instrument) {
-        this.name = name;
-        this.instrument = instrument;
-    }
-    play(action) {
-        return `${this.name} ${action} the ${this.instrument}`;
-    }
-}
-const Page = new Guitarist('Jimmy', 'guitar');
-console.log(Page.play('strums'));
-////////////////////////////////////////////
-class Peeps {
-    static getCount() {
-        return Peeps.count;
-    }
-    constructor(name) {
-        this.name = name;
-        this.name = name;
-        this.id = ++Peeps.count;
-    }
-}
-Peeps.count = 0;
-const John = new Peeps('John');
-const Steve = new Peeps('Steve');
-const Amy = new Peeps('Amy');
-console.log(Peeps.count);
-console.log(Steve.id);
-///////////////////////////////////////////
-class Bands {
-    constructor() {
-        this.datasState = [];
-    }
-    get data() {
-        return this.datasState;
-    }
-    set data(value) {
-        if (typeof value === 'string') {
-            this.datasState.push(value);
-            return;
-        }
-        else
-            throw new Error('Wrong value type');
-    }
-}
-const myBands = new Bands();
-myBands.data = ('Linkin Park');
-console.log(myBands.data);
+//Index Signatures
+const todaysTransactions = {
+    Pizza: -10,
+    Books: -5,
+    Job: 50
+};
+console.log(todaysTransactions.Pizza);
+console.log(todaysTransactions['Pizza']);
+let prop = 'Pizza';
+console.log(todaysTransactions[prop]);
--- src/main.ts
+++ src/main.ts
@@ -1,106 +1,18 @@
-// class Coder{
-//     secondLang!:string
-//    constructor(public readonly name:string,public music:string,private age:number,protected lang:string = 'Typescript'){
-//     this.name = name;
-//     this.music = music;
-//     this.age = age;
-//     this.lang = lang;
-//    }
+//Index Signatures
 
-//    public getAge(){
-//     return `Hello I'am ${this.age}`
-//    }
-// }
-
-
-// const Dave = new Coder('Dave','Rock',42);
-// // console.log(Dave.getAge());
-// // console.log(Dave.lang);
-
-
-// class WebDev extends Coder{
-// constructor(public computer:string,name:string,music:string,age:number){
-//    super(name,music,age);
-//    this.computer = computer;
-// }
-// public getLang(){
-//   return `I write ${this.lang}`;
-// }
-// }
-
-// const Sara = new WebDev('Mac','Sara','Linkin Park',25);
-
-// console.log(Sara.getLang());
-// //console.log(Sara.lang)
-
-
-interface Musician{
-  name:string,
-  instrument:string,
-  play(action:string):string
+interface TransactionObj{
+  [index:string]:number
 }
 
-class Guitarist implements Musician{
-   name:string
-   instrument:string
-
-   constructor(name:string,instrument:string){
-    this.name = name;
-    this.instrument = instrument;
-   }
-
-   play(action: string): string {
-     return `${this.name} ${action} the ${this.instrument}`
-   }
+const todaysTransactions:TransactionObj = {
+  Pizza:-10,
+  Books: -5,
+  Job:50
 }
 
-const Page = new Guitarist('Jimmy','guitar');
+console.log(todaysTransactions.Pizza);
+console.log(todaysTransactions['Pizza']);
 
-console.log(Page.play('strums'));
+let prop:string = 'Pizza';
 
-////////////////////////////////////////////
-
-class Peeps{
-  static count:number = 0;
-
-  static getCount(): number{
-    return Peeps.count
-  }
-  public id:number
-  constructor(public name:string){
-     this.name = name;
-     this.id = ++Peeps.count;
-  }
-}
-
-const John = new Peeps('John');
-const Steve = new Peeps('Steve');
-const Amy = new Peeps('Amy');
-
-console.log(Peeps.count);
-console.log(Steve.id)
-
-///////////////////////////////////////////
-
-class Bands{
-  private datasState: string[]
-
-  constructor(){
-    this.datasState = [];
-  }
-  public get data(): string[]{
-    return this.datasState
-  }
-  public set data(value:string){
-    if(typeof value === 'string'){
-      this.datasState.push(value);
-       return
-    }else throw new Error('Wrong value type')
-  }
-}
-
-const myBands = new Bands();
-
-myBands.data = ('Linkin Park');
-
-console.log(myBands.data)
+console.log(todaysTransactions[prop]);
