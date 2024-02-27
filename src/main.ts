af3882f003a3c2372ad0c0db0b8a6f31bb4494af
Luka Nozadze <nozadzeluka2002@gmail.com>
2024-02-27 17:59:49 +0400

    cross-instance private access

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,24 @@
-class Base {
-  protected x: number = 1;
-}
-class Derived1 extends Base {
-  protected x: number = 5;
-}
-class Derived2 extends Base {
-  f1(other: Derived2) {
-    other.x = 10;
-  }
-  f2(other: Derived1) {
-    other.x = 10;
+// class Base {
+//   protected x: number = 1;
+// }
+// class Derived1 extends Base {
+//   protected x: number = 5;
+// }
+// class Derived2 extends Base {
+//   f1(other: Derived2) {
+//     other.x = 10;
+//   }
+//   f2(other: Derived1) {
+//     other.x = 10;
+//   }
+// }
+
+
+class A {
+  private x = 10;
+ 
+  public sameAs(other: A) {
+    // No error
+    return other.x === this.x;
   }
 }
